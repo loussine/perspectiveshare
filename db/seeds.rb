5f0350aea127d6ff6a14244a3305e17b453e14da
Yolande Touchet <yolandetouchet@hotmail.com>
2024-04-09 19:49:26 +0200

    Création de la seed avec 2 users et 2 posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+p "Deleting current database..."
+
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+p "Current database deleted!"
+
+
+p "Creating users..."
+
+User.create!(nickname: "Ulysse", email: "ulysse@yopmail.com", password: "ulysse")
+User.create!(nickname: "Uma", email: "uma@yopmail.com", password: "umauma")
+
+p "#{User.all.length} users created!"
+
+
+p "Creating posts..."
+
+Post.create!(
+  title: "First Post",
+  content: "This is the content of the first post.",
+  url: "https://example.com/first-post",
+  user: User.first
+)
+
+Post.create!(
+  title: "Second Post",
+  content: "This is the content of the second post.",
+  url: "https://example.com/second-post",
+  user: User.second
+)
+
+p "#{Post.all.length} posts created!"
